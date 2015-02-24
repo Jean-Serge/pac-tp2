bdcf68f8a6688caceb9b6480ccef6a3a266842e4
Jean-Serge <j.monbailly@orange.Fr>
2015-02-24 11:46:22 +0100

    Travail continué, début de récupération du padding.

--- padding-attack.py
+++ padding-attack.py
@@ -4,10 +4,12 @@
 TP2 PAC - padding-attack
 """
 
-
+from helpers import *
 from client import *
+import base64
+import json
 
-SEED = str(5) 
+SEED = str(1) 
 URL = 'http://pac.bouillaguet.info/TP2/padding-attack/'
 SERVER = Server(URL)
 
@@ -16,13 +18,22 @@ SERVER = Server(URL)
 # Récupération du message chiffré
 # -------------------------
 
-# ciphertext + IV
+# ciphertext + IV (Initialization Vector)
 response = SERVER.query('challenge/monbailly/' + SEED) 
 print(response)
 
 
+mess = Message(response['ciphertext']) # block.encode() - ligne 42
+# length = mess.__len__()
+# print(mess.__getitem__(length -1))
+
+dic = {'ciphertext':str(mess)}
+dic = json.dumps(dic)
+print(dic)
 
 
+response = SERVER.query('oracle/monbailly', dic)
+print(response)
 
 
 
